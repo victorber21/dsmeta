51818759e5ce7027e4d9bf1b5df9ad584f900d09
Victor Bernardo <victorber21@gmail.com>
2022-09-11 20:04:48 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/servicess/SaleService.java
@@ -0,0 +1,21 @@
+package com.devsuperior.dsmeta.servicess;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List <Sale> findSale() { 
+		return repository.findAll();
+	}
+
+}
